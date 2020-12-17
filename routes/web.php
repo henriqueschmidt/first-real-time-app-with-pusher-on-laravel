7bbe250927e81567c0eccb3ab304a4b6cf2a616f
Henrique <henriquevetterschmidt@gmail.com>
2020-12-17 11:49:31 -0300

    add chat

--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::view('/users', 'users.showAll')->name('users.all');
 
-
 Route::view('/game', 'game.show')->name('game.show');
+
+Route::get('/chat', 'ChatController@showChat')->name('chat.show');
+Route::post('/chat/message', 'ChatController@messageReceived')->name('chat.message');
